2881a99cc50b159a4457c02e8ce141962d45ad41
Marshall Paiva <marshallpaiva@hotmail.com>
2019-01-18 14:44:30 -0300

    PUT, DELETE e GET para Cliente

---
+++ src/main/java/com/cursomc/dto/ClienteDTO.java
@@ -0,0 +1,61 @@
+package com.cursomc.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.cursomc.domain.Cliente;
+
+public class ClienteDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	
+	@NotEmpty(message = "Campo Obrigatório")
+	@Length(min = 5, max = 120, message = "O tamanho deve ser entre 5 e 80 caracteres")
+	private String nome;
+	
+	@NotEmpty(message = "Campo Obrigatório")
+	@Email(message = "Email Inválido")
+	private String email;
+	
+	public ClienteDTO() {
+		
+	}
+	
+	public ClienteDTO(Cliente obj) {
+		id = obj.getId();
+		nome = obj.getNome();
+		email = obj.getEmail();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
