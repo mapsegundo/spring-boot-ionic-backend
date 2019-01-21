1e5cf4b4e9ce0a116ae11e65eee010fc35aa4d09
Marshall Paiva <marshallpaiva@hotmail.com>
2019-01-21 11:44:46 -0300

    Validacao customizada: email nao podera se repetir

--- src/main/java/com/cursomc/domain/Cliente.java
+++ src/main/java/com/cursomc/domain/Cliente.java
@@ -7,6 +7,7 @@ import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
+import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -24,6 +25,8 @@ public class Cliente {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	
+	@Column(unique = true)
 	private String email;
 	private String cpfOuCnpj;
 	private Integer tipo;
