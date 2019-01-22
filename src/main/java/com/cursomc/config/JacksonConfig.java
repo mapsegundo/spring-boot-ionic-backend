5f900d2408385ea27a1bfbb7338d3c153cb6f9ea
Marshall Paiva <marshallpaiva@hotmail.com>
2019-01-22 10:17:07 -0300

    Inserindo pedido

---
+++ src/main/java/com/cursomc/config/JacksonConfig.java
@@ -0,0 +1,24 @@
+package com.cursomc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+import com.cursomc.domain.PagamentoComBoleto;
+import com.cursomc.domain.PagamentoComCartao;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Configuration
+public class JacksonConfig {
+	@Bean
+	public Jackson2ObjectMapperBuilder objectMapperBuilder() {
+		Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder() {
+			public void configure(ObjectMapper objectMapper) {
+				objectMapper.registerSubtypes(PagamentoComCartao.class);
+				objectMapper.registerSubtypes(PagamentoComBoleto.class);
+				super.configure(objectMapper);
+			};
+		};
+		return builder;
+	}
+}
