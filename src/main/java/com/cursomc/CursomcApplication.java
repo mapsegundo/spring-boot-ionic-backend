9da5a2d9db45180178ca67c6b08f8c680d263059
Marshall Paiva <marshallpaiva@hotmail.com>
2019-02-04 15:00:13 -0300

    Enviando imagem via endpoint

--- src/main/java/com/cursomc/CursomcApplication.java
+++ src/main/java/com/cursomc/CursomcApplication.java
@@ -1,25 +1,18 @@
 package com.cursomc;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.cursomc.services.S3Service;
-
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
-	@Autowired
-	private S3Service s3Service;
-	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		s3Service.uploadFile("C:\\Users\\Shall\\Downloads\\Identificação de Propriedade do Cliente 04-02-2019.pdf");
 	}
 
 }
--- src/main/java/com/cursomc/services/ClienteService.java
+++ src/main/java/com/cursomc/services/ClienteService.java
@@ -1,5 +1,6 @@
 package com.cursomc.services;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,6 +12,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.cursomc.domain.Cidade;
 import com.cursomc.domain.Cliente;
@@ -33,9 +35,13 @@ public class ClienteService {
 
 	@Autowired
 	private ClienteRepository repo;
+	
 	@Autowired
 	private EnderecoRepository enderecoRepository;
 
+	@Autowired
+	private S3Service s3Service;
+	
 	public Cliente find(Integer id) {
 		
 		UserSS user = UserService.authenticated();
@@ -103,5 +109,9 @@ public class ClienteService {
 			cli.getTelefones().add(objDto.getTelefone3());
 		}
 		return cli;
+	}
+	
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+		return s3Service.uploadFile(multipartFile);
 	}
 }
