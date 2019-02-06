fdae8c8f60b7963f404ab78e999b174ed6ac5609
Marshall Paiva <marshallpaiva@hotmail.com>
2019-02-06 10:15:58 -0300

    Ajustando tamanho da imagem

--- src/main/java/com/cursomc/services/ImageService.java
+++ src/main/java/com/cursomc/services/ImageService.java
@@ -10,6 +10,7 @@ import java.io.InputStream;
 import javax.imageio.ImageIO;
 
 import org.apache.commons.io.FilenameUtils;
+import org.imgscalr.Scalr;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -35,12 +36,11 @@ public class ImageService {
 	}
 
 	public BufferedImage pngToJpg(BufferedImage img) {
-		BufferedImage jpgImage = new BufferedImage(img.getWidth(), img.getHeight(),
-				BufferedImage.TYPE_INT_RGB);
+		BufferedImage jpgImage = new BufferedImage(img.getWidth(), img.getHeight(), BufferedImage.TYPE_INT_RGB);
 		jpgImage.createGraphics().drawImage(img, 0, 0, Color.WHITE, null);
 		return jpgImage;
 	}
-	
+
 	public InputStream getInputStream(BufferedImage img, String extension) {
 		try {
 			ByteArrayOutputStream os = new ByteArrayOutputStream();
@@ -49,5 +49,18 @@ public class ImageService {
 		} catch (IOException e) {
 			throw new FileException("Erro ao ler arquivo");
 		}
+	}
+
+	public BufferedImage cropSquare(BufferedImage sourceImg) {
+		int min = (sourceImg.getHeight() <= sourceImg.getWidth()) ? sourceImg.getHeight() : sourceImg.getWidth();
+		return Scalr.crop(sourceImg, 
+				(sourceImg.getWidth() / 2) - (min / 2),
+				(sourceImg.getHeight() / 2) - (min / 2),
+				min, 
+				min);
+	}
+	
+	public BufferedImage resize(BufferedImage sourceImg, int size) {
+		return Scalr.resize(sourceImg, Scalr.Method.ULTRA_QUALITY, size);
 	}
 }
